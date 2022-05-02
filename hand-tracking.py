c4a8b48c97d3bcb030fc79c9702f5daf8cebba91
Kitashi <rishavrajaryan14@gmail.com>
2022-05-02 18:12:03 +0530

    added frame rate and highlighted base landmark

--- hand-tracking.py
+++ hand-tracking.py
@@ -8,6 +8,9 @@ mpHands = mp.solutions.hands #extracts the hands class from the module
 hands = mpHands.Hands()  #creates an object for storing hands
 mpDraw = mp.solutions.drawing_utils  #for drawing points & lines between points
 
+pTime =0
+cTime = 0
+
 while True:
     success, img = cap.read()
     cv2.flip(img, 10, img);
@@ -17,7 +20,25 @@ while True:
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            # print(handLms)
+            # print("next")
+            for id, lm in enumerate(handLms.landmark) :
+                # print(lm, id);
+                # print("next")
+                h,w,c = img.shape  #provide the size of image
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id, cx, cy)
+                if id == 0:
+                    cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time() #provides current time
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 3,(255,0,255),3)
+
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
